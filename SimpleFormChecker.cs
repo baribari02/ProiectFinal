8d39faec621d757869c006ee798c8130ee2fc1f4
Mihai Bariz <barizmihai95@gmail.com>
2025-07-28 12:25:32 +0300

    Delete SimpleFormChecker.cs

--- SimpleFormChecker.cs
+++
@@ -1,5 +0,0 @@
-public class SimpleFormChecker : IFormChecking
-{
-    public bool ValidateEmail(string email) => email.Contains("@") && email.Contains(".");
-    public bool ValidatePassword(string password) => password.Length >= 6;
-}
