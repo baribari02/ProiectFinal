4c3e3ca9dca78ed0a7e3ffceab4f4265b673854f
Mihai Bariz <barizmihai95@gmail.com>
2025-07-28 11:10:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalProj1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
